f64d53c31fb072d8442d3ac32edaf405e8a0bc2b
Emmanuel M. Achana <achanamosey@gmail.com>
2021-04-02 21:22:32 +0100

    first commit

---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        String[] names = {"Emmanuel Achana", "Kofi Yeboah", "John Wayne", "Bruce Banner", "Bruce Wayne", "Danny Rand", "Jake Peralta", "Luke Cage", "Mike Ross"};
+
+        for(String name: names){
+            System.out.println(name);
+        }
+    }
+}
