bcd3c20a5f4e2138ec4ca1567dd182bfbeb29a09
Emmanuel M. Achana <achanamosey@gmail.com>
2021-04-02 22:05:12 +0100

    Demonstration of Array

--- java/src/App.java
+++ java/src/App.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
 
@@ -6,5 +8,17 @@ public class App {
         for(String name: names){
             System.out.println(name);
         }
+
+        int[] priceOfPen = new int[5];
+	    Scanner in = new Scanner(System.in);
+        
+	    for(int i = 0; i < priceOfPen.length; i++)
+	        priceOfPen[i] = in.nextInt();
+
+	    for(int i = 0;i < priceOfPen.length; i++)
+		    System.out.print(priceOfPen[i] + " ");
+
+        in.close();
+
     }
 }
